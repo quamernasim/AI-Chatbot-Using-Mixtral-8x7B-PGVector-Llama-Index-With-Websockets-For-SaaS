64ba8b2e00cb963661c088291cbcde291bc013fd
quamernasim <quamer23nasim@yahoo.com>
2024-02-25 10:45:28 +0000

    Add test_app.py with WebSocket client code

---
+++ test_app.py
@@ -0,0 +1,21 @@
+import argparse
+import asyncio
+import websockets
+
+async def send_message(url, message):
+    async with websockets.connect(url) as websocket:
+        await websocket.send(message)
+        response = await websocket.recv()
+        return response 
+
+def main(args):
+    response = asyncio.get_event_loop().run_until_complete(send_message(args.url, args.message))
+    print("Response from server:", response)
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument("--url", type=str, default="ws://localhost:8765", help="URL to connect to the WebSocket server")
+argparser.add_argument("--message", type=str, default="Hello, world!", help="Message to send to the server")
+args = argparser.parse_args()
+
+if __name__ == "__main__":
+    main(args)
